d2091f44a8ae1428ab3b4592a0f7e555ffe7cd86
KunSheng <tan_kun_sheng@tech.gov.sg>
2020-10-28 11:17:26 +0800

    Add function to get new unanswered question for each player

--- players.js
+++ players.js
@@ -4,6 +4,7 @@ import { success, failure } from "./libs/response-lib";
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client('498998227385-cvqhp70dbea43jeifi6o7t56g5sbmsa1.apps.googleusercontent.com');
 
+//https://developers.google.com/identity/sign-in/web/backend-auth
 async function verify(token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
@@ -14,9 +15,13 @@ async function verify(token) {
 
 /*
 * AWS_PROFILE=kunsheng sls invoke local --function add-user --path mocks/add-user.json
+* https://www.uuidgenerator.net/version4
 */
 export async function add(event) {
     const data = JSON.parse(event.body);
+    console.log("LOGGING");
+    console.log(data);
+    console.log(data.token);
     const user = await verify(data.token).catch(console.error);
     console.log(user);
     console.log(process.env.playerTableName);
@@ -39,3 +44,60 @@ export async function add(event) {
         return failure({ status: e });
     }
 }
+
+// // /*
+// // * AWS_PROFILE=kunsheng sls invoke local --function get-user --path mocks/get-user.json
+// // */
+// export async function getNewQnsForPlayer(event) {
+//     const data = JSON.parse(event.body);
+//     const user = await verify(data.token).catch(console.error);
+//     if (!user) {
+//         return;
+//     }
+//     const params = {
+//         TableName: process.env.playerTableName,
+//         Key: {
+//             player_sub: user.sub
+//         }
+//     };
+
+//     try {
+//         const result = await dynamoDbLib.call("get", params);
+//         const qns = await getNewQuestion(result.Item.answered);
+//         return success(qns);
+//     } catch (e) {
+//         //return the e msg instead
+//         console.log(e);
+//         return failure({ status: e });
+//     }
+// }
+
+// async function getNewQuestion(answered) {
+//     let params = {
+//         TableName: process.env.questionTableName
+//     };
+//     if (answered && answered.length > 0) {
+//         let exprAttrVals = {};
+//         const filterExprArr = answered.map((single, index) => {
+//             exprAttrVals[`:q${index}`] = single;
+//             return `qId <> :q${index}`;
+//         });
+//         const filterExpr = filterExprArr.join(' AND ');
+//         params = {
+//             TableName: process.env.questionTableName,
+//             FilterExpression: filterExpr,
+//             ExpressionAttributeValues: exprAttrVals
+//         };
+//     }
+
+//     try {
+//         const result = await dynamoDbLib.call("scan", params);
+//         const randomPos = Math.round(Math.random() * (result.Items.length - 1));
+//         const randomItem = result.Items[randomPos];
+//         return success(randomItem);
+//     } catch (e) {
+//         //return the e msg instead
+//         console.log(e);
+//         return failure({ status: e });
+//     }
+// }
--- questions.js
+++ questions.js
@@ -1,6 +1,18 @@
 
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client('498998227385-cvqhp70dbea43jeifi6o7t56g5sbmsa1.apps.googleusercontent.com');
+
+//https://developers.google.com/identity/sign-in/web/backend-auth
+//Refactor into a common lambda function
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: '498998227385-cvqhp70dbea43jeifi6o7t56g5sbmsa1.apps.googleusercontent.com',
+    });
+    return ticket.getPayload();
+}
 
 export async function list() {
     console.log(process.env.questionTableName);
@@ -38,5 +50,62 @@ export async function get(event) {
     } catch (e) {
         console.log(e);
         return failure({ status: false });
+    }
+}
+
+// /*
+// * AWS_PROFILE=kunsheng sls invoke local --function get-qns --path mocks/get-qns.json
+// */
+export async function getNewQnsForPlayer(event) {
+    const data = JSON.parse(event.body);
+    const user = await verify(data.token).catch(console.error);
+    if (!user) {
+        return;
+    }
+    const params = {
+        TableName: process.env.playerTableName,
+        Key: {
+            player_sub: user.sub
+        }
+    };
+
+    try {
+        const result = await dynamoDbLib.call("get", params);
+        const qns = await getNewQuestion(result.Item.answered);
+        return success(qns);
+    } catch (e) {
+        //return the e msg instead
+        console.log(e);
+        return failure({ status: e });
+    }
+}
+
+async function getNewQuestion(answered) {
+    let params = {
+        TableName: process.env.questionTableName
+    };
+    if (answered && answered.length > 0) {
+        let exprAttrVals = {};
+        const filterExprArr = answered.map((single, index) => {
+            exprAttrVals[`:q${index}`] = single;
+            return `qId <> :q${index}`;
+        });
+        const filterExpr = filterExprArr.join(' AND ');
+        params = {
+            TableName: process.env.questionTableName,
+            FilterExpression: filterExpr,
+            ExpressionAttributeValues: exprAttrVals
+        };
+    }
+
+    try {
+        const result = await dynamoDbLib.call("scan", params);
+        const randomPos = Math.round(Math.random() * (result.Items.length - 1));
+        const randomItem = result.Items[randomPos];
+        return success(randomItem);
+    } catch (e) {
+        //return the e msg instead
+        console.log(e);
+        return failure({ status: e });
     }
 }
